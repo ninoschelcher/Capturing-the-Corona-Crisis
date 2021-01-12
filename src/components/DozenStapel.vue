09921fb49fa7c9b057d9177dd33f593e953ffaf0
Nino Schelcher <onin1998@gmail.com>
2021-01-12 15:13:57 +0100

    new boxes + transition

--- src/components/DozenStapel.vue
+++ src/components/DozenStapel.vue
@@ -2,71 +2,125 @@
   <section id="boxmountain">
     <div class="scale"></div>
     <div id="boxes">
-      <p class="milestone">
+      <p id="top">
+        <img src="../../public/assets/PakjesPunt.png" alt="" />
+      </p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+      <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      <div class="smallscreen">
+        <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment2.png" alt="" /></p>
+        <p><img src="../../public/assets/PakjesSegment1.png" alt="" /></p>
+      </div>
+    </div>
+    <div id="milestones">
+      <p data-aos="fade-right" class="milestone">
         Het aantal mensen dat in de bedrijfstak zorg en welzijn werkt in
         Amsterdam (2020)
       </p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
         Met een gemiddelde dooshoogte van 80cm en je deze op elkaar stapelt is
         dit net zo hoog als 40 eiffeltorens op elkaar!
       </p>
-      <p class="milestone">Denk je dat je er al bijna bent?</p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
+        Denk je dat je er al bijna bent?
+      </p>
+      <p data-aos="fade-right" class="milestone">
         Het gemiddelde pakket vervoert 50 procent lucht, er zouden dus meer
         pakketjes mee kunnen. Wel zonde eigenlijk..
       </p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
         Het aantal vastgestelde corona besmettingen was op 1 januari 2021 óók
         800.000
       </p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
         De 6 nullen zijn inmiddels al bereikt, het is wel een grote stapel zo..
       </p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
         Zoveel pakketjes moest PostNL nog verwerken per dag begin december.
       </p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
         Als je de gemiddelde lengte van een doos karton neemt (24cm) en deze
         achter elkaar legt kom je van Utrecht naar Bremen met dit aantal dozen.
       </p>
-      <p class="milestone">Het einde is bijna in zicht!</p>
-      <p class="milestone">
+      <p data-aos="fade-right" class="milestone">
+        Het einde is bijna in zicht!
+      </p>
+      <p data-aos="fade-right" class="milestone">
         Dit aantal staat gelijk aan als 10% van de inwoners in Nederland een
         pakketje zou bestellen.
-      </p>
-      <p id="top"><img src="../../public/assets/doosbegin.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p><img src="../../public/assets/doosdoorloop.png" alt="" /></p>
-      <p class="smallscreen">
-        <img src="../../public/assets/doosdoorloop.png" alt="" />
-      </p>
-      <p class="smallscreen">
-        <img src="../../public/assets/doosdoorloop.png" alt="" />
-      </p>
-      <p class="smallscreen">
-        <img src="../../public/assets/doosdoorloop.png" alt="" />
-      </p>
-      <p class="smallscreen">
-        <img src="../../public/assets/doosdoorloop.png" alt="" />
-      </p>
-      <p class="smallscreen">
-        <img src="../../public/assets/doosdoorloop.png" alt="" />
       </p>
     </div>
   </section>
@@ -81,6 +135,8 @@
 
 <script>
 import * as d3 from "d3";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default {
   name: "DozenStapel",
   mounted() {
@@ -104,6 +160,12 @@ export default {
 
     g.selectAll("text").attr("transform", "translate(10,0)");
   },
+  created() {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+    });
+  },
 };
 </script>
 
@@ -117,20 +179,16 @@ export default {
 #boxmountain #top {
   position: relative;
   right: 40px;
-  top: 8px;
+  top: 6px;
 }
 #boxes {
-  margin-left: -100px;
+  margin-left: 50px;
   height: 60160px;
   position: relative;
   overflow: hidden;
 }
-#boxmountain p {
+#boxmountain #boxes p {
   text-align: center;
-  margin-block-start: 0em;
-  margin-block-end: 0em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
 }
 #boxmountain p img {
   max-width: 80%;
@@ -141,64 +199,56 @@ export default {
   display: none;
 }
 
+#milestones {
+  position: relative;
+  width: 50%;
+  overflow: hidden;
+}
+
 .milestone {
-  position: absolute;
-  width: 80vw;
   font-weight: bold;
-  font-size: 1.8em;
-  max-width: 40%;
+  font-size: 2em;
   margin: 0 auto;
-  text-align: center;
+  max-width: 50%;
+  text-align: left;
+  position: absolute;
+  left: 250px;
 }
 
-.milestone:last-child {
-  top: 47000px;
-}
-
-#boxes p:first-of-type {
+#milestones p:first-of-type {
   top: 1770px;
-  left: 450px;
 }
 
-#boxes p:nth-of-type(2) {
+#milestonesp:nth-of-type(2) {
   top: 5300px;
-  left: 500px;
 }
 
-#boxes p:nth-of-type(3) {
+#milestones p:nth-of-type(3) {
   top: 10610px;
-  left: 450px;
 }
 
-#boxes p:nth-of-type(4) {
+#milestones p:nth-of-type(4) {
   top: 21170px;
-  left: 560px;
 }
-#boxes p:nth-of-type(5) {
+#milestones p:nth-of-type(5) {
   top: 28230px;
-  left: 530px;
 }
 
-#boxes p:nth-of-type(6) {
+#milestones p:nth-of-type(6) {
   top: 35300px;
-  left: 500px;
 }
 
-#boxes p:nth-of-type(7) {
+#milestones p:nth-of-type(7) {
   top: 42370px;
-  left: 500px;
 }
-#boxes p:nth-of-type(8) {
+#milestones p:nth-of-type(8) {
   top: 49400px;
-  left: 510px;
 }
-#boxes p:nth-of-type(9) {
+#milestones p:nth-of-type(9) {
   top: 52970px;
-  left: 510px;
 }
-#boxes p:nth-of-type(10) {
+#milestones p:nth-of-type(10) {
   top: 60000px;
-  left: 610px;
 }
 
 #animation2 {
@@ -236,47 +286,18 @@ export default {
     height: 802px;
   }
 
-  #boxmountain #top {
-    right: 0px;
-  }
   .smallscreen {
     display: block;
   }
 
-  #boxes p:first-of-type {
-    left: 380px;
+  #boxes {
+    margin-left: -60px;
   }
 
-  #boxes p:nth-of-type(2) {
-    left: 330px;
-  }
-
-  #boxes p:nth-of-type(3) {
-    left: 360px;
-  }
-
-  #boxes p:nth-of-type(4) {
-    left: 420px;
-  }
-  #boxes p:nth-of-type(5) {
-    left: 340px;
-  }
-
-  #boxes p:nth-of-type(6) {
-    left: 470px;
-  }
-
-  #boxes p:nth-of-type(7) {
-    left: 380px;
-  }
-  #boxes p:nth-of-type(8) {
-    left: 420px;
-  }
-  #boxes p:nth-of-type(9) {
-    left: 420px;
-  }
-  #boxes p:nth-of-type(10) {
-    left: 380px;
+  .milestone {
+    left: 120px;
+    font-size: 1.7em;
+    max-width: 60%;
   }
 }
 </style>
